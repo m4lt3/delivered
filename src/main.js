b796117765ba7889dae6ad50e61d35944f8b2f42
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-22 03:42:03 +0200

    Add a service worker

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+ import { registerServiceWorker } from '@/utils/ServiceRegister.js';
+ registerServiceWorker();
+
 // Components
 import App from './App.vue'
 
