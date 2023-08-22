7e0fbf9c7ec0803c944b24d34de92ec94af0b786
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-22 03:36:42 +0200

    Add copyright disclaimer

---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer class="d-flex align-center justify-center">
+    <i>"Delivered! A Solo Journaling RPG" was created by <a href="http://www.ragekagerugger.com" target="_blank">Kari Jo "Kage" Freudigmann</a> and she owns all rights</i>
+  </v-footer>
+</template>
