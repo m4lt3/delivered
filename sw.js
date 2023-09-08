933b92c3a3c643494973ca133b6736560640a561
m4lt3 <62383691+m4lt3@users.noreply.github.com>
2023-09-08 10:13:16 +0000

    Deploying to gh-pages from @ m4lt3/delivered@ca980afc0dfdc4904086cbf12050feab9ebb74a9 🚀

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 // mostly copied from https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
+const currentCacheVersion = "v1.0.2";
+
 const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open(currentCacheVersion);
   await cache.addAll(resources);
 };
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open(currentCacheVersion);
   await cache.put(request, response);
 };
 
@@ -20,14 +22,65 @@ const cacheFirst = async (request) => {
   return responseFromNetwork;
 };
 
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cacheKeepList = [currentCacheVersion];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      ""
-    ])
-  );
+  event.waitUntil(async () => {
+    await addResourcesToCache(fileList);
+    await clients.claim()
+  });
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(Promise.all([deleteOldCaches()]));
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(cacheFirst(event.request));
 });
+const fileList = [
+"./Stilu-Light.otf",
+"./Stilu-LightOblique.otf",
+"./Stilu-SemiBold.otf",
+"./Stilu-SemiBoldOblique.otf",
+"./android-chrome-192x192.png",
+"./android-chrome-512x512.png",
+"./apple-touch-icon.png",
+"./browserconfig.xml",
+"./favicon-16x16.png",
+"./favicon-32x32.png",
+"./favicon.ico",
+"./index.html",
+"./mstile-150x150.png",
+"./safari-pinned-tab.svg",
+"./site.webmanifest",
+"./assets/Courier.6781914c.js",
+"./assets/Courier.def452ff.css",
+"./assets/Default.3da96cbd.js",
+"./assets/Default.83a40549.css",
+"./assets/Home.29f2632f.js",
+"./assets/Home.daf19aca.css",
+"./assets/VContainer.9b4c590c.css",
+"./assets/VContainer.e38ad402.js",
+"./assets/VMenu.0d8841cc.js",
+"./assets/VMenu.80acc9b6.css",
+"./assets/_plugin-vue_export-helper.a03f6180.js",
+"./assets/delivered_logo.a2e0386b.png",
+"./assets/heading.0547b151.css",
+"./assets/heading.b63df6f7.js",
+"./assets/index.1d31b8c9.css",
+"./assets/index.71436e4c.js",
+"./assets/materialdesignicons-webfont.48d3eec6.woff",
+"./assets/materialdesignicons-webfont.861aea05.eot",
+"./assets/materialdesignicons-webfont.bd725a7a.ttf",
+"./assets/materialdesignicons-webfont.e52d60f6.woff2",
+];
