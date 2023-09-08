7001ff3031109befab061aa5ba54112048bc2132
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-09-08 10:16:48 +0200

    Add version number and issue link

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-footer class="d-flex align-center justify-center">
-    <i>"Delivered! A Solo Journaling RPG" was created by <a href="http://www.ragekagerugger.com" target="_blank">Kari Jo "Kage" Freudigmann</a> and she owns all rights</i>
+  <v-footer class="d-flex flex-column align-center justify-center">
+    <p>"Delivered! A Solo Journaling RPG" was created by <a href="http://www.ragekagerugger.com" target="_blank">Kari Jo "Kage" Freudigmann</a> and she owns all rights</p>
+    <i>v1.0.1 - <a href="https://github.com/m4lt3/delivered/issues" target="_blank">Send Feedback</a></i>
   </v-footer>
 </template>
