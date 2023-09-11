ebb004d32f13510dea3d7984004338c3d83230ee
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-09-11 14:39:49 +0200

    Change footer link

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer class="d-flex flex-column align-center justify-center">
-    <p>"Delivered! A Solo Journaling RPG" was created by <a href="http://www.ragekagerugger.com" target="_blank">Kari Jo "Kage" Freudigmann</a> and she owns all rights</p>
-    <i>v1.0.2 - <a href="https://github.com/m4lt3/delivered/issues" target="_blank">Send Feedback</a></i>
+    <p>"Delivered! A Solo Journaling RPG" was created by <a href="http://www.ragekagegames.com" target="_blank">Kari Jo "Kage" Freudigmann</a> and she owns all rights</p>
+    <i>v1.0.3 - <a href="https://github.com/m4lt3/delivered/issues" target="_blank">Send Feedback</a></i>
   </v-footer>
 </template>
