1d97f1d102048afb277b592a0c1a666c41550b4e
m4lt3 <m4lt3@users.noreply.github.com>
2023-09-11 12:50:57 +0000

    Deploying to gh-pages from @ m4lt3/delivered@e2869420f7462728bedc26c7d7e5a9e31d642d51 🚀

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // mostly copied from https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
-const currentCacheVersion = "v1.0.2";
+const currentCacheName = "delivered-v1.0.3";
 
 const addResourcesToCache = async (resources) => {
-  const cache = await caches.open(currentCacheVersion);
+  const cache = await caches.open(currentCacheName);
   await cache.addAll(resources);
 };
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open(currentCacheVersion);
+  const cache = await caches.open(currentCacheName);
   await cache.put(request, response);
 };
 
@@ -27,9 +27,9 @@ const deleteCache = async (key) => {
 };
 
 const deleteOldCaches = async () => {
-  const cacheKeepList = [currentCacheVersion];
+  const cacheKeepList = [currentCacheName];
   const keyList = await caches.keys();
-  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  const cachesToDelete = keyList.filter((key) => ( key.match(/^delivered-v.*/) && !cacheKeepList.includes(key)) );
   await Promise.all(cachesToDelete.map(deleteCache));
 };
 
@@ -63,22 +63,22 @@ const fileList = [
 "./mstile-150x150.png",
 "./safari-pinned-tab.svg",
 "./site.webmanifest",
-"./assets/Courier.b8f54074.js",
+"./assets/Courier.aacc785a.js",
 "./assets/Courier.def452ff.css",
-"./assets/Default.34533c43.js",
+"./assets/Default.02528026.js",
 "./assets/Default.83a40549.css",
-"./assets/Home.600c6445.js",
+"./assets/Home.786265ac.js",
 "./assets/Home.daf19aca.css",
 "./assets/VContainer.9b4c590c.css",
-"./assets/VContainer.abe0f964.js",
-"./assets/VMenu.21fe8503.js",
+"./assets/VContainer.ddd21cfd.js",
 "./assets/VMenu.80acc9b6.css",
-"./assets/_plugin-vue_export-helper.a0a4da0b.js",
+"./assets/VMenu.ea262fe9.js",
+"./assets/_plugin-vue_export-helper.b9de9329.js",
 "./assets/delivered_logo.a2e0386b.png",
 "./assets/heading.0547b151.css",
-"./assets/heading.cc310a77.js",
+"./assets/heading.4e27d137.js",
 "./assets/index.1d31b8c9.css",
-"./assets/index.4b35b5f0.js",
+"./assets/index.4c39f74e.js",
 "./assets/materialdesignicons-webfont.48d3eec6.woff",
 "./assets/materialdesignicons-webfont.861aea05.eot",
 "./assets/materialdesignicons-webfont.bd725a7a.ttf",
